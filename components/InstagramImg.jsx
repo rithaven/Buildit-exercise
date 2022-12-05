2e2156a36520e5699544de795a360906f1daf729
rithaven <veneruth@gmail.com>
2022-12-05 16:26:12 +0200

    Add home page

--- components/InstagramImg.jsx
+++ components/InstagramImg.jsx
@@ -5,10 +5,6 @@ const InstagramImg = ({socialImg}) => {
   return (
     <div>
         <Image src={socialImg} alt='/' className='w-full h-full' layout='responsive' />
-        <div className='flex justify-center items-center absolute top-0 left-0 right-0 bottom-0 hover:bg-black/50 group'>
-
-
-        </div>
     </div>
   )
 }
