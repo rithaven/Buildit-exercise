93705f52b653efbd28ec5747f7ecd6dac32e3838
rithaven <veneruth@gmail.com>
2022-12-05 16:32:29 +0200

    Add home page

--- pages/about.js
+++ pages/about.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 function about() {
   return (
-    <div className='text-3xl undeline'>
+    <div className='text-3xl'>
      About
      <h1  className='mt-14'>  This is About Page</h1> 
     </div>
